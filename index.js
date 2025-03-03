68568ca523712cf5e256bb055aab2964270ce04e
yussuf <yusufmim2001@gmail.com>
2025-03-03 20:48:27 +0300

    arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
-
 // Write your code here
+// Declare the variables
+let num1 = 31;
+let num2 = 2;
+let num3 = 10;
+let num4 = 6;
+
+// Multiply num1 and num2 to get 62
+let multiply = num1 * num2;
+
+// Generate a random integer greater than 0
+let random = Math.floor(Math.random() * 100) + 1; 
+
+// Find the remainder when num3 is divided by num4
+let mod = num3 % num4;
+
+// Find the highest number in a set that includes 20
+let max = Math.max(1, 5, 10, 15, 20);
